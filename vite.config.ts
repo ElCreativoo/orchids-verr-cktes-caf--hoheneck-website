f0c4ecace332e1cf94cf932047262bf5fe6183b0
ElCreativoo <sandronajer97@gmail.com>
2026-05-08 11:43:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/orchids-verr-cktes-caf--hoheneck-website/',
   plugins: [react()],
 })
